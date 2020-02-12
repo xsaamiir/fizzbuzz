6deb31c4cb7d450ccf2a03dc2c4289449011a04c
samir elsharkawy <samir.elsharkawy@gmail.com>
2020-02-12 22:36:09 +0100

    http: add logging middleware

--- http/server.go
+++ http/server.go
@@ -22,10 +22,10 @@ func New(port string, logger *log.Logger, metrics metrics.FizzBuzzMetrics) http.
 	mux.HandleFunc("/metrics", hs.handleMetrics)
 
 	return http.Server{
-		Addr:         port,                   // configure the bind address
-		Handler:      mux,                    // set the default handler
-		ReadTimeout:  httpServerReadTimeout,  // max time to read request from the client
-		WriteTimeout: httpServerWriteTimeout, // max time to write response to the client
-		IdleTimeout:  httpServerIdleTimeout,  // max time for connections using TCP Keep-Alive
+		Addr:         port,                           // configure the bind address
+		Handler:      loggingMiddleware(logger, mux), // set the default handler
+		ReadTimeout:  httpServerReadTimeout,          // max time to read request from the client
+		WriteTimeout: httpServerWriteTimeout,         // max time to write response to the client
+		IdleTimeout:  httpServerIdleTimeout,          // max time for connections using TCP Keep-Alive
 	}
 }
