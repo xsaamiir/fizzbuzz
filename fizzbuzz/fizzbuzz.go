93c295ea1313c452f8cd6c0c9116e21ccc387fc5
samir elsharkawy <samir.elsharkawy@gmail.com>
2020-02-13 21:03:50 +0100

    fizzbuzz: rename n1 -> int1 & n2 -> int2

--- fizzbuzz/fizzbuzz.go
+++ fizzbuzz/fizzbuzz.go
@@ -9,18 +9,18 @@ import (
 // 	- all multiples of int1 are replaced by str1
 // 	- all multiples of int2 are replaced by str2
 // 	- all multiples of int1 and int2 are replaced by str1str2.
-func FizzBuzz(n1, n2, limit int, str1, str2 string) []string {
+func FizzBuzz(int1, int2, limit int, str1, str2 string) []string {
 	res := make([]string, limit)
 
 	for i := 1; i <= limit; i++ {
 		var s string
 
 		switch {
-		case i%n1 == 0 && i%n2 == 0:
+		case i%int1 == 0 && i%int2 == 0:
 			s = str1 + str2
-		case i%n1 == 0:
+		case i%int1 == 0:
 			s = str1
-		case i%n2 == 0:
+		case i%int2 == 0:
 			s = str2
 		default:
 			s = strconv.Itoa(i)
